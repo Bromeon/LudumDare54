448e8f3ac117db6b01eaa4dc802d025412e4788a
Jan Haller <bromeon@gmail.com>
2023-09-30 13:00:00 +0200

    Supply ships, universe and paths

--- Scenes/Main/Main.gd
+++ Scenes/Main/Main.gd
@@ -3,9 +3,19 @@ extends Node2D
 @onready
 var sprite := $Icon
 
+@onready
+var supply_paths := $SupplyPaths
+
 func _ready():
 	print("MMO initializing...")
 
 
 func _process(delta):
 	sprite.rotate(delta * 1.5)
+
+	for path in supply_paths.get_children():
+		var follow: PathFollow2D = path.get_node("PathFollow2D")
+		follow.progress_ratio += delta * 0.01
+		
+	
+			
