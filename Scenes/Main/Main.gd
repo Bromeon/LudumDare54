a59fd30a57ae3bbc00cd8b020c58a7e1c698c667
Jan Haller <bromeon@gmail.com>
2023-09-30 13:19:01 +0200

    2 paths, proper pos/rot initialization

--- Scenes/Main/Main.gd
+++ Scenes/Main/Main.gd
@@ -9,6 +9,19 @@ var supply_paths := $SupplyPaths
 func _ready():
 	print("MMO initializing...")
 
+	for path in supply_paths.get_children():
+		var follow: PathFollow2D = path.get_node("PathFollow2D")
+		var ship: Node2D = follow.get_node("SupplyShip")
+		var curve: Curve2D = path.curve
+		
+		var cur = curve.get_point_position(0)
+		var next = curve.get_point_position(1)
+		var direction = (next - cur).angle() + deg_to_rad(90)
+		
+		ship.global_position = cur
+		ship.global_rotation = direction
+		
+		
 
 func _process(delta):
 	sprite.rotate(delta * 1.5)
