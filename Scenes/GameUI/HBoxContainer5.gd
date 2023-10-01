618f7b9961e069f01a4635335601d35aa75f014a
setzer22 <jsanchezfsms@gmail.com>
2023-10-01 19:56:12 +0200

    Tooltips on quest panel too

--- Scenes/GameUI/HBoxContainer5.gd
+++ Scenes/GameUI/HBoxContainer5.gd
@@ -6,5 +6,6 @@ func _ready():
 		for row in vbox.get_children():
 			var mineral = GameConstants.MINERAL_NAMES[i]
 			row.get_node("TextureRect").modulate = GameConstants.MINERAL_TYPE_COLORS[mineral]
+			row.get_node("TextureRect").mineral = mineral # Set tooltip
 			row.get_node("Label").set_text("x " + str(GameConstants.QUEST_REQUIREMENTS[mineral]))
 			i += 1
