c24e5e141d48a44ee7cdbf04dbbf79bb840e9a58
setzer22 <jsanchezfsms@gmail.com>
2023-10-01 18:07:43 +0200

    Remove viewport thing that didn't work

--- Scenes/Game/SubViewportContainer.gd
+++
@@ -1,9 +0,0 @@
-extends SubViewportContainer
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var size = get_viewport_rect().size
-	var sizei = Vector2i(size)
-	if $SubViewport.size != sizei:
-		$SubViewport.size = sizei
