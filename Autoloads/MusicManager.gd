1762277c89daffe4338d5bd1fe253668b874cdfa
setzer22 <jsanchezfsms@gmail.com>
2023-10-02 22:55:36 +0200

    Release

--- Autoloads/MusicManager.gd
+++ Autoloads/MusicManager.gd
@@ -1,6 +1,10 @@
 extends Node
 
+var played: bool = false
+
 func play_victory_music():
-	$AudioStreamPlayer.stream = preload("res://Assets/Audio/Neon Laser Horizon.mp3")
-	$AudioStreamPlayer.playing = true
+	if not played:
+		played = true
+		$AudioStreamPlayer.stream = preload("res://Assets/Audio/Neon Laser Horizon.mp3")
+		$AudioStreamPlayer.playing = true
 
